7b4837026a592001506f92b334d5baf7c4b4fd05
wangbingshuai <274694451@qq.com>
2019-08-09 08:22:45 +0800

    仿写im

---
+++ src/main/java/com/wbs/simpledemo/im/IOClient.java
@@ -0,0 +1,33 @@
+package com.wbs.simpledemo.im;
+
+/**
+ * @description: TODO
+ * @author: wangbingshuai
+ * @create: 2019-08-08 23:54
+ **/
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Date;
+
+/**
+ * @author 闪电侠
+ */
+public class IOClient {
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            try {
+                Socket socket = new Socket("127.0.0.1", 8000);
+                while (true) {
+                    try {
+                        socket.getOutputStream().write((new Date() + ": hello world").getBytes());
+                        Thread.sleep(2000);
+                    } catch (Exception e) {
+                    }
+                }
+            } catch (IOException e) {
+            }
+        }).start();
+    }
+}
